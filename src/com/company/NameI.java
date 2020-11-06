6fbdebdb246defce20fa785bedeb7354aae0151b
dmint <dkosimov@gmail.com>
2020-11-06 11:30:18 +0100

    MD program

---
+++ src/com/company/NameI.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class NameI {
+    String vName;
+    Integer vValue;
+
+    public NameI() {
+        vName = "";
+        vValue = 0;
+    }
+}
