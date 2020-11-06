40da0edc139bf4a5d62d4b2992fa3668009981fd
dmint <dkosimov@gmail.com>
2020-11-06 11:33:12 +0100

    MD program

---
+++ src/com/company/UCell.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class UCell {
+    ArrayList<Integer> initUcell = new ArrayList<>();
+
+    public UCell() {
+        initUcell = null;
+    }
+}
