da194c27bd1a30c79f3480e46468fed9155325f2
dmint <dkosimov@gmail.com>
2020-11-15 18:44:52 +0100

    Input?Output 2

--- src/com/company/InitCoords.java
+++ src/com/company/InitCoords.java
@@ -4,6 +4,6 @@ public class InitCoords {
     VecR c = new VecR();
     VecR gap = new VecR();
     int n, nx, ny, nz;
-    SetParams setParams = new SetParams();
-    setParams.VDiv(gap, c, c);
+    com.company.SetParams setParams = new com.company.SetParams();
+
 }
