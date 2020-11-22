73c1054c9e962b5a99547fb7bbc789adca86e487
dkmint <dkosimov@gmail.com>
2020-11-22 15:43:32 +0100

    SetParams Class

--- src/com/company/SetParams.java
+++ src/com/company/SetParams.java
@@ -1,6 +1,9 @@
 package com.company;
 
 public class SetParams {
+    VecR r = new VecR();
+    VecR rv = new VecR();
+//    double x, y, z;
     public static void VSCopyR(VecR v2, double s1, VecI v1) {
         v2.x = s1 * v1.x;
         v2.y = s1 * v1.y;
@@ -30,15 +33,18 @@ public class SetParams {
         v1.y = v2.y * v3.y;
         v1.z = v2.z * v3.z;
     }
-    public static void VSAdd(VecR v1, VecR v2, double s3, VecR v3) {
-        v1.x = v2.x + s3 + v3.x;
-        v1.y = v2.y + s3 + v3.y;
-        v1.z = v2.z + s3 + v3.z;
+    public void VSAdd(VecR v2, double s3, VecR v3) {
+        this.rv.x = v2.x + s3 * v3.x;
+        this.rv.y = v2.y + s3 * v3.y;
+        this.rv.z = v2.z + s3 * v3.z;
     }
-    public static void VVSAdd(VecR v1, double s2, VecR v2) {
-        v1.x = v1.x + s2 + v2.x;
-        v1.y = v1.y + s2 + v2.y;
-        v1.z = v1.z + s2 + v2.z;
+    public void VVSAdd(VecR r, double s2, VecR v2) {
+        this.r.x = r.x + s2 * v2.x;
+        this.r.y = r.y + s2 * v2.y;
+        this.r.z = r.z + s2 * v2.z;
     }
+//    public void VVSAdd(VecR v1, double s2, VecR v2) {
+//        VSAdd(v1, v1, s2, v2);
+//    }
 }
 
