330700c07348874c9cf1f9ee0f65e1f8d1edd472
dkmint <dkosimov@gmail.com>
2020-11-24 21:00:39 +0100

    VCSum(VecR) method added

--- src/com/company/Prop.java
+++ src/com/company/Prop.java
@@ -14,15 +14,17 @@ public class Prop {
         this.sum /= n;
         this.sum2 = Math.sqrt(Math.max(this.sum2 / (double) n - (this.sum * this.sum), 0.));
     }
-    public double getPropEst (int flag) {
-        if (flag == 1)
-            return this.sum;
-        else if (flag == 2)
-            return this.sum2;
-        else
-            System.out.printf("Error! Flag value must be 1 or 2");
-        return 0;
-    }
+//    public void getPropsEst() {
+//    }
+//    public double getPropEst (int flag) {
+//        if (flag == 1)
+//            return this.sum;
+//        else if (flag == 2)
+//            return this.sum2;
+//        else
+//            System.out.printf("Error! Flag value must be 1 or 2");
+//        return 0;
+//    }
 
 }
 
